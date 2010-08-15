0e032521521043017a25ccbf529c09369dadc494
Kevin Locke <klocke@digitalenginesoftware.com>
2010-08-14 22:40:08 -0600

    Update distutils metadata
    Add trove classifiers (somewhat arbitrarily).
    Add Windows to the platform list.
    Change license from 'LGPL' to 'GNU LGPL' (because it is listed in the
    enumeration of valid licenses in PEP 241 which defines the PKG-INFO
    format that the data is used to generate).
    
    Signed-off-by: Kevin Locke <klocke@digitalenginesoftware.com>

--- setup.py
+++ setup.py
@@ -11,14 +11,27 @@ providing access to control GnuPG via versatile and extensible means.
 This module is based on GnuPG::Interface, a Perl module by the same author.
 """
 
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+    'Operating System :: Microsoft :: Windows :: Windows NT/2000',
+    'Operating System :: POSIX',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Topic :: Security :: Cryptography',
+    'Topic :: Software Development :: Libraries',
+]
+
 distutils.core.setup( name = 'GnuPGInterface',
                       version = GnuPGInterface.__version__,
                       description = 'GnuPG interactions with file handles',
 		      long_description = long_description,
                       author = 'Frank J. Tobin',
                       author_email = 'ftobin@neverending.org',
-		      license = 'LGPL',
-		      platforms = 'POSIX',
+		      license = 'GNU LGPL',
+		      platforms = ['POSIX', 'Windows'],
+		      classifiers = classifiers,
 		      keywords = 'GnuPG gpg',
                       url = 'http://py-gnupg.sourceforge.net/',
                       py_modules = [ 'GnuPGInterface' ]
